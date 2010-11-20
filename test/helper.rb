84177434fe624e14c128bf924cf6faec9463b492
Padraig O'Sullivan <osullivan.padraig@gmail.com>
2010-11-20 14:57:42 -0500

    Added new helper file for unit tests.

---
+++ test/helper.rb
@@ -0,0 +1,26 @@
+require 'rubygems'
+
+require File.join(File.dirname(__FILE__), *%w[.. lib drizzle])
+
+require 'test/unit'
+require 'shoulda'
+require 'rr'
+
+include Drizzle
+
+class Test::Unit::TestCase
+  include RR::Adapters::TestUnit
+
+  def dest_dir(*subdirs)
+    File.join(File.dirname(__FILE__), 'dest', *subdirs)
+  end
+
+  def source_dir(*subdirs)
+    File.join(File.dirname(__FILE__), 'source', *subdirs)
+  end
+
+  def clear_dest
+    FileUtils.rm_rf(dest_dir)
+  end
+
+end
